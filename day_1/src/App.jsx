177d2c3a1bea8972ba192055c145362352faa7c3
karan-raval <karanraval424@gmail.com>
2024-07-08 18:41:37 +0530

    second app

--- day_1/src/App.jsx
+++ day_1/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img src="" alt="" />
+      <img src="https://cdn.pixabay.com/photo/2024/03/04/16/38/cat-8612685_960_720.jpg" alt="" />
     </div>
   );
 }
---
+++ day_3_state_propt/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./components/navbar";
+import Slider from './components/Slider';
+
+function App() {
+  return (
+    <div>
+     <Navbar></Navbar>
+     <Slider/>
+    </div>
+  );
+}
+
+export default App;
