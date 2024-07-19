11356bd9c7de7bbb85e644de6cc6171bf7754e29
karan-raval <karanraval424@gmail.com>
2024-07-19 22:02:58 +0530

    product page

--- productpage/src/App.jsx
+++ productpage/src/App.jsx
@@ -22,7 +22,7 @@ const handleclick=()=>{
     <nav className="custom-navbar navbar navbar navbar-expand-md navbar-dark " arial-label="Furni navigation bar">
 
 			<div className="container">
-				<a className="navbar-brand" href="index.html">Furni<span>.</span></a>
+				<a className="navbar-brand" href="index.html">Accessories<span>.</span></a>
 
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
