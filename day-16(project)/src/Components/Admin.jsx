16e4ac3ce8b95752342fe7568db6a79f663097f2
karan-raval <karanraval424@gmail.com>
2024-08-07 13:13:29 +0530

    Day-16 Admin Panel with Local Storage

--- day-16(project)/src/Components/Admin.jsx
+++ day-16(project)/src/Components/Admin.jsx
@@ -1,106 +1,166 @@
 import React from "react";
-import Table from 'react-bootstrap/Table';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect, useState } from 'react' onChange={handleChange} value={title} name='title' 
+import Table from "react-bootstrap/Table";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 const Admin = () => {
-  const [state,setState] = useState({
-    title : "",
-    price : "",
-    des : "",
-    image : ""
- })
- const [arr,setArr] = useState(JSON.parse(localStorage.getItem("data")) || [])
-  const handleChange = (e)=>{
-          let {name,value} = e.target
-          setState({...state,[name] : value})
-  } 
- const handleSubmit = (e)=>{
-     e.preventDefault()
-     setArr([...arr,state])
-     setState({
-      title : "",
-      price : "",
-      des : "",
-      image : ""
-     })
- }
- useEffect(()=>{
-  localStorage.setItem("data",JSON.stringify(arr))
- },[arr])
- const handleDelete = (title)=>{
-     let a = [];
-     for(let x=0; x<arr.length; x++){
-      if(arr[x].title != title){
-        a.push(arr[x])
-      }
+  const [state, setState] = useState({
+    id:uuidv4(),
+    title: "",
+    bname: "",
+    price: "",
+    dprice: "",
+    oprice: "",
+    des: "",
+    image: "",
+  });
+  const [arr, setArr] = useState(
+    JSON.parse(localStorage.getItem("data")) || []
+  );
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setArr([...arr, state]);
+    setState({
+      id:uuidv4(),
+      title: "",
+      bname: "",
+      price: "",
+      dprice: "",
+      oprice: "",
+      des: "",
+      image: "",
+    });
+  };
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(arr));
+  }, [arr]);
+
+
+  const handleDelete = (ID)=>{
+    let a = [];  
+    for(let x=0; x<arr.length; x++){
+     if(arr[x].id != ID){
+       a.push(arr[x])
      }
-     setArr(a)
- }
- let {title,price,des,image} = state
+    }
+    setArr(a)
+  
+   }
+  let { title, dprice, oprice, bname, des, image } = state;
   return (
     <>
-    <div className="body">
-    <div class="wrapper ">
-    <div class="title">
-      Admin Panel
-    </div>
-    
-    <div class="form">
-      <form >
-      <div class="input_field">
-        <input type="text" onChange={handleChange} value={title} name='title'  placeholder="Enter Product Title" class="input"/>
-      </div>
-      <div class="input_field">
-        <input type="text" onChange={handleChange} value={price} name='price'  placeholder="Enter Product Brand Name" class="input"/>
-      </div>
-      <div class="input_field">
-        <input type="text" onChange={handleChange} value={title} name='title'  placeholder="Enter Product Image URL" class="input"/>
-      </div>
-      <div class="input_field">
-        <input type="text" onChange={handleChange} value={title} name='title'  placeholder="Enter Product Discount Price" class="input"/>
-      </div>
-      <div class="input_field">
-        <input type="text" onChange={handleChange} value={title} name='title'  placeholder="Enter Product Orignal Price" class="input"/>
-      </div>
-      <div class="input_field">
-        <input type="text" onChange={handleChange} value={title} name='title'  placeholder="Enter Product catagory" class="input"/>
-      </div>
-      <div class="input_field">
-      <input type="submit"  class="btn"/>
-      </div>
-    </form>
-    </div>
-  </div>
-  </div>
+      <div className="body">
+        <div className="wrapper ">
+          <div className="title">Admin Panel</div>
 
-    <div className="data">
-  <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th>Product Title</th>
-          <th>Product Brand Name</th>
-          <th>Product Image URL</th>
-          <th>Product Discount Price</th>
-          <th>Product Orignal Price</th>
-          <th>Product catagory</th>
-          <th>Delet</th>
-          <th>Edit</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-          <td>@mdo</td>
-          <td>@mdo</td>
-        </tr>
-        
-         
-      </tbody>
-    </Table>
-    </div>
+          <div className="form">
+            <form onSubmit={handleSubmit}>
+              <div className="input_field">
+                <input
+                  type="text"
+                  onChange={handleChange}
+                  value={title}
+                  name="title"
+                  placeholder="Enter Product Title"
+                  className="input"
+                />
+              </div>
+              <div className="input_field">
+                <input
+                  type="text"
+                  onChange={handleChange}
+                  value={bname}
+                  name="bname"
+                  placeholder="Enter Product Brand Name"
+                  className="input"
+                />
+              </div>
+              <div className="input_field">
+                <input
+                  type="text"
+                  onChange={handleChange}
+                  value={image}
+                  name="image"
+                  placeholder="Enter Product Image URL"
+                  className="input"
+                />
+              </div>
+              <div className="input_field">
+                <input
+                  type="text"
+                  onChange={handleChange}
+                  value={dprice}
+                  name="dprice"
+                  placeholder="Enter Product Discount Price"
+                  className="input"
+                />
+              </div>
+              <div className="input_field">
+                <input
+                  type="text"
+                  onChange={handleChange}
+                  value={oprice}
+                  name="oprice"
+                  placeholder="Enter Product Orignal Price"
+                  className="input"
+                />
+              </div>
+              <div className="input_field">
+                <input
+                  type="text"
+                  onChange={handleChange}
+                  value={des}
+                  name="des"
+                  placeholder="Enter Product Description"
+                  className="input"
+                />
+              </div>
+              <div className="input_field">
+                <input type="submit" className="btn" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <div className="data">
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Product Title</th>
+              <th>Product Brand Name</th>
+              <th>Product Image URL</th>
+              <th>Product Discount Price</th>
+              <th>Product Orignal Price</th>
+              <th>Product catagory</th>
+              <th>Delet</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tbody>
+            {arr.map((el) => {
+              return (
+                <tr>
+                  <td>Product ID:-{el.id}</td>
+                  <td>{el.title}</td>
+                  <td>{el.bname}</td>
+                  <td> <img src={el.image} alt={el.title} /></td>
+                  <td>{el.dprice}</td>
+                  <td>{el.oprice}</td>
+                  <td>{el.des}</td>
+                  <td><button onClick={()=>handleDelete(el.id)}>Delete</button></td>
+                  <td><button onClick={()=>handlEdit(el.title)}>edit</button></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
     </>
   );
 };
